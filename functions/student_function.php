b62fae1db7f15446c91f5f9e9763610e983f003d
xmartina <martinsprince126@gmail.com>
2024-08-31 03:50:17 +0100

    d

--- functions/student_function.php
+++ functions/student_function.php
@@ -10,11 +10,67 @@ session_start();
 // Include the shared database connection
 $ROOT_DIR = '/home/multistream6/domains/thomas-back.matagram.com/public_html/';
 include ($ROOT_DIR . 'functions/main_function.php'); // Adjust the path based on your directory structure
+
 if(!isset($_SESSION['student_id'])){ ?>
 <script>// Redirect to a specific URL
     window.location.href = '<?=$site_link ?>auth/login'; // Replace with your target URL
 </script>
 <?php }
+
 $student_id = $_SESSION['student_id'];
+
+$query = "SELECT * FROM students WHERE id = :student_id";
+$stmt = $conn->prepare($query);
+$stmt->execute([
+    ':student_id' => $student_id
+]);
+$student = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($student) {
+    $stu_fname = $student['fname'];
+    $stu_lname = $student['lname'];
+    $stu_email = $student['email'];
+    $stu_phone = $student['phone_number'];
+    $stu_department = $student['department_id'];
+    switch ($stu_department) {
+        case 1:
+            $stu_department = 'Electrical/Electronics Engineering';
+            break;
+        case 2:
+            $stu_department = 'Computer Engineering';
+            break;
+        case 3:
+            $stu_department = 'Computer Science';
+            break;
+        case 4:
+            $stu_department = 'Statistics';
+            break;
+        case 5:
+            $stu_department = 'Health Information Management';
+            break;
+        case 6:
+            $stu_department = 'Pharmaceutical Technology';
+            break;
+        case 7:
+            $stu_department = 'Community/Public Health';
+            break;
+        case 8:
+            $stu_department = 'Medical Laboratory Science';
+            break;
+        case 9:
+            $stu_department = 'Accountancy';
+            break;
+        case 10:
+            $stu_department = 'Business Administration';
+            break;
+        case 11:
+            $stu_department = 'Public Administration';
+            break;
+        default:
+            $stu_department = 'Unknown Department'; // Handle unexpected values
+            break;
+    }
+
+}
 
 ?>
