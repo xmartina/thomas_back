6727a39836d542e74a3446beea8c1aa8c8b9ec2a
xmartina <martinsprince126@gmail.com>
2024-08-31 03:32:50 +0100

    d

--- student/partials/header.php
+++ student/partials/header.php
@@ -1,3 +1,3 @@
 <?php
 $ROOT_DIR = '/home/multistream6/domains/thomas-back.matagram.com/public_html/';
-include_once($ROOT_DIR . 'student/partials/header.php');
+include_once($ROOT_DIR . 'functions/student_function.php');
