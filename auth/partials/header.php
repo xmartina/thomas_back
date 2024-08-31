c20408273ba038eaf999d8a924bacfc02802a629
xmartina <martinsprince126@gmail.com>
2024-08-31 02:09:27 +0100

    d

--- auth/partials/header.php
+++ auth/partials/header.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en" dir="ltr">
-
+<?php $file_dir = $ROOT_DIR . 'back_end'; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +10,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- inject:css-->
-    <link rel="stylesheet" href="./assets/vendor/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="<?= $file_dir ?>./assets/vendor/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="<?= $file_dir ?>assets/css/style.css">
     <!-- endinject -->
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon.svg">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= $file_dir ?>assets/img/favicon.svg">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.0.8/css/line.min.css">
 
--- functions/main_function.php
+++ functions/main_function.php
@@ -1,3 +1,4 @@
 <?php
 // Correctly include the shared database connection from the include folder
 include '../include/db_connection.php'; // Adjust the path based on your directory structure
+$ROOT_DIR = '/home/multistream6/domains/thomas-back.matagram.com/public_html/';
